4964719f88ba9d40d6b82750d4cd661e957c60a5
himankBellpost <himank@bellpost.com>
2025-02-12 17:35:19 +0530

    Test: Sum unknown number of values

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,7 +3,6 @@ import 'package:string_calculator_dart/string_calculator.dart';
 
 void main() {
   group('StringCalculator.add', () {
-
     // Empty String
     test('returns 0 for empty string', () {
       expect(StringCalculator.add(''), equals(0));
@@ -14,9 +13,12 @@ void main() {
       expect(StringCalculator.add('1'), equals(1));
     });
 
-
     test('sums two comma-separated numbers', () {
       expect(StringCalculator.add('1,2'), equals(3));
+    });
+
+    test('sums unknown number of values', () {
+      expect(StringCalculator.add('1,2,3,4'), equals(10));
     });
   });
 }
