a282d52c3525587d80c48f40943e96bd154402ae
himankBellpost <himank@bellpost.com>
2025-02-12 17:21:18 +0530

    Implement single number parsing in add()

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
-void main(List<String> arguments) {}
+import 'dart:io';
+
+import 'package:string_calculator_dart/string_calculator.dart';
+
+void main(List<String> arguments) {
+  try {
+    // Prompt the user for input
+    stdout.write('Enter numbers: ');
+    String? input = stdin.readLineSync();
+
+    // Check if input is null or empty
+    if (input == null || input.isEmpty) {
+      print('No input provided. Exiting.');
+      return;
+    }
+
+    int result = StringCalculator.add(input);
+
+    print('The result is: $result');
+  } catch (e) {
+    print('Error: $e');
+  }
+}
--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,12 @@
 class StringCalculator {
   static int add(String numbers) {
     if (numbers.isEmpty) return 0;
+
+    // If the input is a single number
+    if (!numbers.contains(',') && !numbers.contains('\n')) {
+      return int.parse(numbers);
+    }
+
     return 0;
   }
 }
