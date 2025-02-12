b4596d45348180604b7cf29e859367aa7dfca12f
himankBellpost <himank@bellpost.com>
2025-02-12 17:14:49 +0530

    Test: Return number for single input

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,8 +3,15 @@ import 'package:string_calculator_dart/string_calculator.dart';
 
 void main() {
   group('StringCalculator.add', () {
+
+    // Empty String
     test('returns 0 for empty string', () {
       expect(StringCalculator.add(''), equals(0));
+    });
+
+    // Single Number
+    test('returns the number for a single number input', () {
+      expect(StringCalculator.add('1'), equals(1));
     });
   });
 }
